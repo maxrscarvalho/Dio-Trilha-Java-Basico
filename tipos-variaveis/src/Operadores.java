cd06f719a98331a0ed4e1806cf024c688156f699
Max Carvalho <120032100+maxrscarvalho@users.noreply.github.com>
2023-03-15 21:39:46 -0300

    projeto Smart TV

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,15 @@
+public class Operadores {
+    public static void main (String args[]){
+        boolean condicao1 = true;
+
+        boolean condicao2 = false;
+
+        if (condicao1 && condicao2){
+            System.out.println("As duas condiçoes é verdadeira");
+        }
+
+        if (condicao1 || condicao2){
+            System.out.println("Uma das duas condiçoes é verdadeira");
+        }
+    }
+}
