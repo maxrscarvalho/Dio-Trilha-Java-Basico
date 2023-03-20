12c7429a9c2d826090745c1fc2106620dfe16e36
Max Carvalho <120032100+maxrscarvalho@users.noreply.github.com>
2023-03-20 14:43:47 -0300

    comentarios

--- Java-Terminal/src/Comentarios.java
+++ Java-Terminal/src/Comentarios.java
@@ -1,10 +1,14 @@
-// Olá, eu sou um comentário em uma única linha
+public class Comentarios {
+    public static void main (String args[]){
+  
+}
 /*
- * Este método foi elaborado as pressas
- * por isso eu abrevei o nome das variáveis
- * mas olha, ele tem a finalidade de somar ou  multiplicar
- * dois números
- * /
+ *Este método foi elaborado as pressas
+* por isso eu abrevei o nome das variáveis
+* mas olha, ele tem a finalidade de somar ou  multiplicar
+* dois números
+ */
+
 public int somaMultiplica (int n, int x, String m){
     int r = 0; // r é igual ao resultado
     if (m == "M"){ // M= multiplicação
@@ -15,3 +19,5 @@ public int somaMultiplica (int n, int x, String m){
     }
     return r;
 }
+
+
